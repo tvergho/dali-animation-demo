a3e7c1628342dd5f3ab3483c3af50c487203da6d
Tyler Vergho <tvergho@gmail.com>
2021-05-04 14:29:57 -0700

    Drag example

--- src/Framer.js
+++ src/Framer.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { motion, useAnimation, transform } from 'framer-motion';
 
-// Influenced by https://www.framer.com/api/motion/examples/ and
+// Influenced by:
+// https://www.framer.com/api/motion/examples/
 // https://codesandbox.io/s/framer-motion-spring-powered-characters-remaining-count-i1wgk
+// https://codesandbox.io/s/9tin5
 
 const mapRemainingToColor = transform([10, 2], ['#ff008c', '#ccc']);
 const mapRemainingToSpringVelocity = transform([10, 0], [50, 0]);
@@ -27,30 +29,46 @@ const Framer = () => {
 
   return (
     <div className="framer">
-      <motion.div
-        animate={{
-          scale: [1, 2, 2, 1, 1, 1],
-          rotate: [0, 0, 270, 270, 0, 0],
-          borderRadius: ['20%', '20%', '50%', '50%', '20%', '20%'],
-        }}
-        transition={{ repeat: Infinity, duration: 1.5 }}
-        className="object"
-      />
-      <motion.div
-        animate={controls}
-        style={{ color: mapRemainingToColor(value) }}
-        className="object-3"
-      >
-        {value}
-      </motion.div>
-      <motion.div
-        animate={{
-          y: [0, -50, 0],
-          backgroundColor: ['rgb(74, 156, 250)', 'rgb(84, 180, 88)', 'rgb(74, 156, 250)'],
-        }}
-        transition={{ repeat: Infinity }}
-        className="object-2"
-      />
+      <div className="framer-item">
+
+        <motion.div
+          animate={{
+            scale: [1, 2, 2, 1, 1, 1],
+            rotate: [0, 0, 270, 270, 0, 0],
+            borderRadius: ['20%', '20%', '50%', '50%', '20%', '20%'],
+          }}
+          transition={{ repeat: Infinity, duration: 1.5 }}
+          className="purple-scale"
+        />
+      </div>
+      <div className="framer-item">
+        <motion.div
+          animate={controls}
+          style={{ color: mapRemainingToColor(value) }}
+          className="counter"
+        >
+          {value}
+        </motion.div>
+      </div>
+      <div className="framer-item">
+        <motion.div
+          animate={{
+            y: [0, -50, 0],
+            backgroundColor: ['rgb(74, 156, 250)', 'rgb(84, 180, 88)', 'rgb(74, 156, 250)'],
+          }}
+          transition={{ repeat: Infinity }}
+          className="bounce"
+        />
+      </div>
+      <div className="drag-constraint">
+        <motion.div
+          className="square"
+          whileHover={{ scale: 1.05 }}
+          drag="x"
+          dragConstraints={{ left: -150, right: 150 }}
+          dragElastic={0.1}
+        />
+      </div>
     </div>
   );
 };
