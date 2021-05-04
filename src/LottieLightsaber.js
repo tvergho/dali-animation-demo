e1dfa444c8294f1461a09b8a8472cf985923ccbf
Tyler Vergho <tvergho@gmail.com>
2021-05-04 11:46:42 -0700

    Documentation and meta updates

--- src/LottieLightsaber.js
+++ src/LottieLightsaber.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Lottie from 'react-lottie';
-import animationData from './lightsaber3.json';
+import animationData from './animations/lightsaber3.json';
 
 const defaultOptions = {
   loop: true,
